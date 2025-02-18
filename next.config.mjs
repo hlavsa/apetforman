f5e27b94face963e1cfe39cf75df87d3abc8f06d
okraj <hlavsa.jara@seznam.cz>
2025-02-18 12:58:11 +0100

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    appDir: true, // Ensure this is set for App Router
-  },
   reactStrictMode: true,
-  swcMinify: true,
 };
 
 export default nextConfig;
