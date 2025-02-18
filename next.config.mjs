9fa6900cd07f4f8c17ac68747c23e7892cc9b245
okraj <hlavsa.jara@seznam.cz>
2025-02-18 19:04:46 +0100

    fix

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-};
+  output: 'export',
+  distDir: 'build',
+  images: {
+    unoptimized: true,
+  },
+}
 
 export default nextConfig;
