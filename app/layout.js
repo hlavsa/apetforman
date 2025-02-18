ac58bf6f477586113ef22d1527c5a929991adf92
okraj <hlavsa.jara@seznam.cz>
2025-02-18 12:48:35 +0100

    vercel

--- app/layout.js
+++ app/layout.js
@@ -1,32 +1,7 @@
-// app/layout.js
-import "./globals.css";
-import { Reenie_Beanie } from "next/font/google";
-import React from "react";
-
-import { IBM_Plex_Serif } from 'next/font/google';
-
-const ibmPlexSerif = IBM_Plex_Serif({
-  subsets: ['latin', 'latin-ext'],
-  weight: ['500']
-});
-
-const reenieBeanie = Reenie_Beanie({
-  weight: "400",
-  subsets: ["latin"],
-  display: "swap",
-});
-
-export const metadata = {
-  title: "Petra Formánková",
-  description: "Sample torn-paper page with separate torn links",
-};
-
 export default function RootLayout({ children }) {
   return (
-    <html lang="cs" className={reenieBeanie.className}>
-      <body>
-        {children}
-      </body>
+    <html lang="cs">
+      <body>{children}</body>
     </html>
-  );
+  )
 }
